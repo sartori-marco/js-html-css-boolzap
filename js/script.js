5b91e563d68972141fe3f387d7f0afccd6dc7004
marco sartori <marcosartori98@yahoo.com>
2021-02-02 11:04:48 +0100

    aggiunto avatar e nome contatto con Vue

--- js/script.js
+++ js/script.js
@@ -1,15 +1,45 @@
 var app = new Vue({
-  el:'#root',
+  el:'#app',
   data: {
     users: [
       {
         nome: 'Michele',
-        avatar: 'img/avatar_1.jpg'
+        avatar: 'img/avatar_1.jpg',
+        messaggio:[
+          {
+            testo: 'ciao',
+            stato: 'inviato'
+          }
+        ]
       },
       {
-        nome: 'Michele',
-        avatar: 'img/avatar_1.jpg'
-      }
+        nome: 'Fabio',
+        avatar: 'img/avatar_2.jpg'
+      },
+      {
+        nome: 'Alessia',
+        avatar: 'img/avatar_3.jpg'
+      },
+      {
+        nome: 'Luca',
+        avatar: 'img/avatar_4.jpg'
+      },
+      {
+        nome: 'Stefano',
+        avatar: 'img/avatar_5.jpg'
+      },
+      {
+        nome: 'Sara',
+        avatar: 'img/avatar_6.jpg'
+      },
+      {
+        nome: 'Alberto',
+        avatar: 'img/avatar_7.jpg'
+      },
+      {
+        nome: 'Giorgio',
+        avatar: 'img/avatar_8.jpg'
+      },
     ]
   }
 
