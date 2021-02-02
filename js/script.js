c6b4ca0461d0a40c621d877ece14ac075114fbb9
marco sartori <marcosartori98@yahoo.com>
2021-02-02 10:51:29 +0100

    inizio vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+var app = new Vue({
+  el:'#root',
+  data: {
+    users: [
+      {
+        nome: 'Michele',
+        avatar: 'img/avatar_1.jpg'
+      },
+      {
+        nome: 'Michele',
+        avatar: 'img/avatar_1.jpg'
+      }
+    ]
+  }
 
+
+
+});
